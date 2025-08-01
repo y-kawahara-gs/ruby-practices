7f1eacc15b90c5ee7ec19748952e4bd46ae69dec
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-08-01 09:50:12 +0900

    カレンダープログラムの作成

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'date'
+
+month = 0
+year = 0
+
+opt = OptionParser.new
+
+opt.on('-m int'){|v| month = v.to_i}
+opt.on('-y int'){|v| year = v.to_i}
+
+opt.parse!(ARGV)
+day = DateTime.now
+
+if month == 0 && year == 0
+  month = day.month
+  year = day.year
+elsif year == 0
+  year = day.year
+end
+
+date_f = Date.new(year, month, 1)
+date_l = Date.new(year, month, -1)
+
+puts "#{month}月 #{year}".center(21)
+puts "日 月 火 水 木 金 土"
+
+wday_f = date_f.wday
+blank = "   "*wday_f
+print blank
+
+(date_f..date_l).each do |x|
+  if x.strftime('%a') == "Sat" && x.strftime('%-d').to_i < 10
+    print " #{x.strftime('%-d')}\n"
+  elsif x.strftime('%a') == 'Sat'
+    print "#{x.strftime('%-d')}\n"
+  elsif x.strftime('%-d').to_i < 10
+    print " #{x.strftime('%-d')} "
+  else
+    print "#{x.strftime('%-d')} "
+  end
+end
+
