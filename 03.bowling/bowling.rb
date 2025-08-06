42ce7de36c9a615106513dff9367de8f1c213b3b
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-08-06 09:51:19 +0900

    メソッドの削除・分岐の修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,25 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def strike(st_frame, st)
-  if st_frame[st][0] == 10
-    if st_frame[st + 1][0] == 10
-      20 + st_frame[st + 2][0]
-    else
-      10 + st_frame[st + 1].sum
-    end
-  else
-    0
-  end
-end
-
-def spare(sp_frame, sp)
-  if sp_frame[sp].sum == 10 && sp_frame[sp][0] != 10
-    10 + sp_frame[sp + 1][0]
-  else
-    0
-  end
-end
 
 def open_frame(op_frame, op)
   if op_frame[op].sum != 10
@@ -38,10 +19,22 @@ frames = shots.each_slice(2).to_a
 point = 0
 frames.each_with_index do |frame, index|
   next if index > 9
-
-  point += strike(frames, index)
-  point += spare(frames, index)
-  point += open_frame(frames, index)
+  if frames[index].sum == 10
+    point += frames[index].sum
+    if frames[index][0] == 10
+      if frames[index + 1][0] == 10
+        point += frames[index + 1][0] + frames[index + 2][0]
+      else
+        point += frames[index + 1].sum
+      end
+    end
+    if frames[index][0] != 10
+      point += frames[index + 1][0]
+    end
+  end
+  if frames[index].sum != 10
+    point += frames[index].sum
+  end
 end
 
 puts point
