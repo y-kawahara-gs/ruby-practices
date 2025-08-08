a42a2589e58aa6a69b7898935a546797ef23d700
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-08-08 10:48:12 +0900

    .sumメソッドの追加、分岐処理の修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+require 'debug'
 
 score = ARGV[0]
 scores = score.split(',')
@@ -7,25 +8,18 @@ shots = scores.flat_map { |s| s == 'X' ? [10, 0] : s.to_i }
 
 frames = shots.each_slice(2).to_a
 
-point = 0
-frames.each_with_index do |frame, index|
-  next if index > 9
-  if frames[index].sum == 10
-    point += frames[index].sum
-    if frames[index][0] == 10
-      point += frames[index + 1][0]
-      if frames[index + 1][0] == 10
-        point += frames[index + 2][0]
-      else
-        point += frames[index + 1][1]
-      end
+point = frames.take(10).each_with_index.sum do |frame, index|
+  if frames[index].sum < 10
+    frames[index].sum
+  elsif frames[index][0] != 10
+    frames[index].sum + frames[index + 1][0]
+  else
+    if frames[index + 1][0] == 10
+      frames[index].sum + frames[index + 1].sum + frames[index + 2][0]
     else
-      point += frames[index + 1][0]
+      frames[index].sum +frames[index + 1].sum
     end
-  else frames[index].sum != 10
-    point += frames[index].sum
   end
 end
-
 puts point
 
