78b2a02fed0a79784bad0cd0285aebc9acff317f
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-09-09 07:59:14 +0900

    -lオプションの実装

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,35 +1,98 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 require 'optparse'
+require 'etc'
+require 'date'
 
-def contents
-  opt = OptionParser.new
-  option = {}
-  opt.on('-a') { |v| option[:a] = v }
-  opt.on('-r') { |v| option[:r] = v }
-  opt.parse!(ARGV)
-
-  option_a = (option[:a] == true ? File::FNM_DOTMATCH : 0)
-  files = Dir.glob("*", option_a)
-  files = files.reverse if option[:r]
-  rows = files.length.ceildiv(3)
-  file_rows = Array.new(rows) { Array.new(3) }
-
-  files.each_with_index do |file, index|
-    row = index % rows
-    col = index / rows
-    file_rows[row][col] = file
-  end
-  file_rows
+def main
+  Contents.option
+  Contents.details
+  Display.print_ls
 end
 
-def print_ls
-  contents.each do |file_rows|
-    file_rows.each do |file|
-      print "#{file}  "
+class Contents
+  @@option = {}
+  def self.option
+    opt = OptionParser.new
+    opt.on('-a') { |v| @@option[:a] = v }
+    opt.on('-r') { |v| @@option[:r] = v }
+    opt.on('-l') { |v| @@option[:l] = v }
+    opt.parse!(ARGV)
+  end
+
+  def self.details
+    option_a = (@@option[:a] == true ? File::FNM_DOTMATCH : 0)
+    files = Dir.glob("*", option_a)
+    files = files.reverse if @@option[:r]
+    
+    rows = files.length.ceildiv(3)
+    file_rows = Array.new(rows) { Array.new(3) }
+    files.each_with_index do |file, index|
+      row = index % rows
+      col = index / rows
+      file_rows[row][col] = file
     end
-    print "\n"
+    @@option[:l] == true ? files : file_rows
   end
 end
 
-print_ls
+class Display < Contents
+  def self.print_ls
+    if @@option[:l]
+      Display.total
+      details.each do |file|
+        mode = File.stat(file).mode.to_s(8)
+
+        mode.gsub!(/14+(\d{4})/, 's\\1')
+        mode.gsub!(/12+(\d{4})/, 'l\\1')
+        mode.gsub!(/10+(\d{4})/, '-\\1')
+        mode.gsub!(/6+(\d{4})/, 'b\\1')
+        mode.gsub!(/4+(\d{4})/, 'd\\1')
+        mode.gsub!(/2+(\d{4})/, 'c\\1')
+        mode.gsub!(/1+(\d{4})/, 'p\\1')
+
+        mode.gsub!(/0+(\d{3})/, '\\1')
+
+        2.downto(0) do |p|
+          mode.gsub!(/0+(\d{#{p}})$/, '---\\1')
+          mode.gsub!(/1+(\{{#{p}})$/, '--x\\1')
+          mode.gsub!(/2+(\d{#{p}})$/, '-w-\\1')
+          mode.gsub!(/3+(\d{#{p}})$/, '-wx\\1')
+          mode.gsub!(/4+(\d{#{p}})$/, 'r--\\1')
+          mode.gsub!(/5+(\d{#{p}})$/, 'r-x\\1')
+          mode.gsub!(/6+(\d{#{p}})$/, 'rw-\\1')
+          mode.gsub!(/7+(\d{#{p}})$/, 'rwx\\1')
+        end
+        
+        link = File.stat(file).nlink
+        owner = Etc.getpwuid(File.stat(file).uid).name
+        group = Etc.getpwuid(File.stat(file).gid).name
+        byte = File.stat(file).size
+        time = File.stat(file).mtime
+
+        print "#{mode} #{link} #{owner} #{group} "
+        print "#{byte} ".rjust(5)
+        print "#{time.to_date.strftime('%b')}"
+        print "#{time.to_date.strftime('%e')} ".rjust(4)
+        print "#{time.strftime('%H')}:#{time.strftime('%M')} "
+        print "#{file}\n"
+      end
+    else
+      details.each do |file_rows|
+        file_rows.each do |file|
+          print "#{file}  "
+        end
+        print "\n"
+      end
+    end
+  end
+
+  def self.total
+    total_size = Contents.details.each.sum do |one_size|
+      File.stat(one_size).blocks
+    end
+    puts "total #{total_size/2}"
+  end
+end
+
+main
