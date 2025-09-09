8286266b4b615fdbc90df56a5681ed1b31c0126b
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-09-09 13:51:50 +0900

    権限処理をメソッド化

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -37,33 +37,11 @@ def display(**options)
   opt = options
   if options[:l]
     total(**opt)
+
     details(**opt).each do |file|
       info = File.stat(file)
       mode = info.mode.to_s(8)
-
-      mode.insert(0, '0') if mode.length == 5
-
-      case mode[0, 2]
-      when '14' then print 's'
-      when '12' then print 'l'
-      when '10' then print '-'
-      when '06' then print 'b'
-      when '04' then print 'd'
-      when '02' then print 'c'
-      end
-
-      for num in 3..5 do
-        case mode[num]
-        when '0' then print '---'
-        when '1' then print '--x'
-        when '2' then print '-w-'
-        when '3' then print '-wx'
-        when '4' then print 'r--'
-        when '5' then print 'r-x'
-        when '6' then print 'rw-'
-        when '7' then print 'rwx'
-        end
-      end
+      permission(mode)
 
       link = info.nlink
       owner = Etc.getpwuid(info.uid).name
@@ -94,6 +72,31 @@ def total(**options)
     File.stat(one_size).blocks
   end
   puts "total #{total_size/2}"
+end
+
+def permission(mode)
+  mode.insert(0, '0') if mode.length == 5
+  case mode[0, 2]
+  when '14' then print 's'
+  when '12' then print 'l'
+  when '10' then print '-'
+  when '06' then print 'b'
+  when '04' then print 'd'
+  when '02' then print 'c'
+  end
+  
+  for num in 3..5 do
+    case mode[num]
+    when '0' then print '---'
+    when '1' then print '--x'
+    when '2' then print '-w-'
+    when '3' then print '-wx'
+    when '4' then print 'r--'
+    when '5' then print 'r-x'
+    when '6' then print 'rw-'
+    when '7' then print 'rwx'
+    end
+  end
 end
 
 main
