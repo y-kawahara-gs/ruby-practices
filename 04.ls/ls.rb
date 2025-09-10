23d4be4811940b53a9dcc67d39fe27a3c24dd9cf
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-09-10 10:37:41 +0900

    lオプションの分岐変更とrubocop解消

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -23,22 +23,16 @@ def get_file(**options)
   option_a = (options[:a] ? File::FNM_DOTMATCH : 0)
   files = Dir.glob('*', option_a)
   files = files.reverse if options[:r]
-  rows = files.length.ceildiv(3)
-  file_rows = Array.new(rows) { Array.new(3) }
-  files.each_with_index do |file, index|
-    row = index % rows
-    col = index / rows
-    file_rows[row][col] = file
-  end
-  options[:l] ? files : file_rows
+  files
 end
 
 def display(**options)
   opt = options
+  files = get_file(**opt)
   if options[:l]
-    total_size = get_file(**opt).each.sum { |one_size| File.stat(one_size).blocks }
+    total_size = files.each.sum { |one_size| File.stat(one_size).blocks }
     puts "total #{total_size / 2}"
-    get_file(**opt).each do |file|
+    files.each do |file|
       info = File.stat(file)
       mode = info.mode.to_s(8)
       mode.insert(0, '0') if mode.length == 5
@@ -47,12 +41,7 @@ def display(**options)
       l_option(info, file)
     end
   else
-    get_file(**opt).each do |file_rows|
-      file_rows.each do |file|
-        print "#{file}  "
-      end
-      print "\n"
-    end
+    no_l_option(files)
   end
 end
 
@@ -99,6 +88,22 @@ def l_option(info, file)
   print "#{time.strftime('%e')} ".rjust(4)
   print time.strftime('%R')
   print " #{file}\n"
+end
+
+def no_l_option(files)
+  rows = files.length.ceildiv(3)
+  file_rows = Array.new(rows) { Array.new(3) }
+  files.each_with_index do |file, index|
+    row = index % rows
+    col = index / rows
+    file_rows[row][col] = file
+  end
+  file_rows.each do |rows|
+    rows.each do |file|
+      print "#{file}  "
+    end
+    print "\n"
+  end
 end
 
 main
