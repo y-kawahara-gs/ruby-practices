fea58a5c35476eb86364ffb0d3af49812df4bc50
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-09-16 10:01:27 +0900

    標準入力を受け取った時の処理を修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -19,22 +19,26 @@ end
 
 def display(**options)
   opt = options
-  all = { lines: 0, words: 0, capacity: 0 }
-  paths = wc_paths
-  paths.each do |path|
-    load_contents = Array.new(0)
-    contents = File.read(path)
-    contents.split(' ').each_with_index { |word, index| load_contents[index] = word }
-    print_wc_details(contents.count("\n"), load_contents.size, File.size(path), **opt)
-    print "#{path}\n"
-    all[:lines] += contents.count("\n")
-    all[:words] += load_contents.size
-    all[:capacity] += File.size(path)
-  end
-  return unless paths.size != 1
+  if ARGV == []
+    print_standard(**opt)
+  else
+    all = { lines: 0, words: 0, capacity: 0 }
+    paths = ARGV
+    paths.each do |path|
+      load_contents = Array.new(0)
+      contents = File.read(path)
+      contents.split(' ').each_with_index { |word, index| load_contents[index] = word }
+      print_wc_details(contents.count("\n"), load_contents.size, File.size(path), **opt)
+      print "#{path}\n"
+      all[:lines] += contents.count("\n")
+      all[:words] += load_contents.size
+      all[:capacity] += File.size(path)
+    end
+    return unless paths.size != 1
 
-  print_wc_details(all[:lines], all[:words], all[:capacity], **opt)
-  print 'total'
+    print_wc_details(all[:lines], all[:words], all[:capacity], **opt)
+    print 'total'
+  end
 end
 
 def print_wc_details(lines, words, capacity, **opt)
@@ -52,18 +56,16 @@ def print_wc_details(lines, words, capacity, **opt)
   end
 end
 
-def wc_paths
-  paths = ARGV
-  if ARGV == []
-    while (path = $stdin.gets)
-      paths << path.chomp.split(' ')
-    end
-    max_size = paths.map(&:size).max
-    paths.map! { |it| it.values_at(0..max_size) }
-    paths.transpose.flatten.compact
-  else
-    paths
+def print_standard(**options)
+  opt = options
+  line_break = 0
+  byte = 0
+  while (path = $stdin.gets)
+    line_break += 1
+    byte += path.bytesize
+    ARGV << path.chomp.split(' ')
   end
+  print_wc_details(line_break, ARGV.flatten.size, byte, **opt)
 end
 
 main
