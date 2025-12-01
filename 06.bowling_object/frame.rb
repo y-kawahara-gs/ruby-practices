c7eda2eae893d45ce0bfad90179fd143ae1d4f50
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-12-02 08:46:45 +0900

    bonus_scoreをreturn処理の追加・ネストの削除

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -16,16 +16,13 @@ class Frame
   end
 
   def score(next_frame, after_next_frame)
-    bonus_score = if !next_frame.nil?
-                    if spare?
-                      next_frame.first_shot.score
-                    elsif strike?
-                      bonus = next_frame.first_shot.score + next_frame.second_shot.score
-                      bonus += after_next_frame.first_shot.score if next_frame.strike? && !after_next_frame.nil?
-                      bonus
-                    else
-                      0
-                    end
+    return base_score if next_frame.nil?
+    bonus_score = if spare?
+                    next_frame.first_shot.score
+                  elsif strike?
+                    bonus = next_frame.first_shot.score + next_frame.second_shot.score
+                    bonus += after_next_frame.first_shot.score if next_frame.strike? && !after_next_frame.nil?
+                    bonus
                   else
                     0
                   end
